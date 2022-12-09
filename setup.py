80b540582eff1691f46f7d6e10aacd1b15120a7a
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-09 07:56:29 +0000

    update setup and requirements

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import find_packages, setup
 setup(
     name='r2de',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0',
     description='Module for the estimation of difficulty and discrimination of multiple-choice questions from text',
     author='Luca Benedetto',
     license='',
     install_requires=[
-        "matplotlib==3.1.2",
-        "nltk==3.4.5",
-        "numpy==1.18.1",
-        "pandas==0.25.3",
+        "nltk==3.7",
+        "numpy==1.23.5",
+        "pandas==1.5.2",
         "pyirt==0.3.4",
-        "scikit-learn==0.22.1",
+        "scikit-learn==1.1.3",
+        "matplotlib==3.6.2",
     ],
 )
